28845609838cecb39e165ac20137d55f7b97c37d
GFROCRAI <gwenda.frocrain@gmail.com>
2016-01-17 00:59:23 +0100

    set job controller

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -9,7 +9,7 @@ before_action :set_cv, only: [:show, :edit, :update, :destroy]
   end
 
   def new           # GET /cvs/new
-    @cv = Restaurant.new
+    @cv = Cv.new
   end
 
   def create        # POST /cvs
@@ -28,31 +28,31 @@ before_action :set_cv, only: [:show, :edit, :update, :destroy]
     @cv.update(params[:cv])
     # Will raise ActiveModel::ForbiddenAttributesError
 
-    # no need for app/views/restaurants/create.html.erb
+    # no need for app/views/cvs/create.html.erb
     redirect_to cv_path(@cv)
   end
 
   def destroy       # DELETE /cvs/:id
     @cv.destroy
 
-    # no need for app/views/restaurants/destroy.html.erb
+    # no need for app/views/cvs/destroy.html.erb
     redirect_to cvs_path
 
-    # <%= link_to "Delete", restaurant_path(restaurant),
+    # <%= link_to "Delete", cv_path(cv),
     #   method: :delete,
     #   data: { confirm: "Are you sure?" } %>
   end
 
   private
 
-  def restaurant_params
+  def cv_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:cv).permit(:job, :first_name, :last_name, :email, :city, :experience, :start_date, :comment)
   end
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
+  def set_cv
+    @cv = Cv.find(params[:id])
   end
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,57 @@
 class JobsController < ApplicationController
+before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
     @job = Job.all
   end
 
   def show
+  end
+
+  def new           # GET /jobs/new
+    @job = Job.new
+  end
+
+  def create        # POST /jobs
+    @job = Job.new(params[:job])
+    @job.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to job_path(@job)
+  end
+
+  def edit          # GET /jobs/:id/edit
+  end
+
+  def update        # PATCH /jobs/:id
+    @job.update(params[:job])
+    # Will raise ActiveModel::ForbiddenAttributesError
+
+    # no need for app/views/jobs/create.html.erb
+    redirect_to job_path(@job)
+  end
+
+  def destroy       # DELETE /jobs/:id
+    @job.destroy
+
+    # no need for app/views/jobs/destroy.html.erb
+    redirect_to jobs_path
+
+    # <%= link_to "Delete", job_path(job),
+    #   method: :delete,
+    #   data: { confirm: "Are you sure?" } %>
+  end
+
+  private
+
+  def job_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:job).permit(:firm_name, :firm_type, :firm_email, :proposed_job, :alternativ_job, :job_city, :job_start_date, :comment)
+  end
+
+  def set_job
     @job = Job.find(params[:id])
   end
 
