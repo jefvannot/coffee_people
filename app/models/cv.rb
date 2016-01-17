4d89e16676bc8654c3bb58d89e14d168a4ea6462
GFROCRAI <gwenda.frocrain@gmail.com>
2016-01-17 01:03:39 +0100

    set up validations in models

--- app/models/cv.rb
+++ app/models/cv.rb
@@ -1,2 +1,6 @@
 class Cv < ActiveRecord::Base
+  validates :job, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :city, presence: true
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+  validates :firm_name, presence: true
+  validates :firm_email, presence: true
+  validates :proposed_job, presence: true
+  validates :job_city, presence: true
+
 end
